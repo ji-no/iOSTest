a0865074bf29ee3413764d9e5c6836b2f04cf86d
jichu.nobuyuki <jichu.nobuyuki@vega-c.com>
2019-03-02 10:12:34 +0900

    Initial commit

---
+++ iOSTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSTest
+//
+//  Created by 寺中 信行 on 2019/03/02.
+//  Copyright © 2019 寺中 信行. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
